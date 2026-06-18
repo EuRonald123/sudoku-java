a443dabc6b96b5afa1356eadcd9875ba4ade8292
Ronald_Ubuntu <ronaldscosta0708@gmail.com>
2026-06-17 21:15:54 -0300

    mudanca de args para usar um Enum e adicao de opcoes de dificuldade do tabuleiro

--- src/main/java/com/sudoku/App.java
+++ src/main/java/com/sudoku/App.java
@@ -2,8 +2,7 @@ package com.sudoku;
 
 import com.sudoku.control.GameController;
 import com.sudoku.model.Board;
-import com.sudoku.model.Space;
-//import com.sudoku.view.MenuTerminal;
+import com.sudoku.util.BoardPreset;
 import com.sudoku.view.WindowSudokuUI;
 
 import javax.swing.*;
@@ -11,35 +10,25 @@ import javax.swing.*;
 public class App {
 
     public static void main(String[] args) {
-        
-        if (args.length != 81) {
-            System.out.println("Error: The game needs exactly 81 arguments to start.");
-            return; 
+        BoardPreset preset;
+
+        if (args.length > 0) {
+            preset = BoardPreset.valueOf(args[0].toUpperCase());
+        } else {
+            preset = (BoardPreset) JOptionPane.showInputDialog(
+                null,
+                "Choose a difficulty:",
+                "Sudoku",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                BoardPreset.values(),
+                BoardPreset.EASY
+            );
+            if (preset == null) return;
         }
 
-        Space[][] grid = new Space[9][9];
-
-        for (String arg : args) {
-            
-            String[] partes = arg.split(";");            
-            String[] coordenadas = partes[0].split(","); 
-            String[] dados = partes[1].split(",");       
-
-            //converte
-            int column = Integer.parseInt(coordenadas[0]);
-            int line = Integer.parseInt(coordenadas[1]);
-            
-            //gabarito
-            int expectedValue = Integer.parseInt(dados[0]); 
-            boolean fixo = Boolean.parseBoolean(dados[1]);
-
-            grid[line][column] = new Space(expectedValue, fixo);
-        }
-
-        Board board = new Board(grid);
+        Board board = preset.createBoard();
         GameController controller = new GameController(board);
         SwingUtilities.invokeLater(() -> new WindowSudokuUI(800, 800, board, controller));
-        // MenuTerminal menu = new MenuTerminal(board, controller);
-        // menu.start();
     }
 }
